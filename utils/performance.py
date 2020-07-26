4d2a61ba1b6af4843217b2cfa56c2f4139a26f2d
Ahmed Alaa <a7med3laa@hotmail.com>
2020-07-26 17:17:40 +0200

    synthetic and real data experiments

---
+++ utils/performance.py
@@ -0,0 +1,26 @@
+
+
+# Copyright (c) 2020, Ahmed M. Alaa
+# Licensed under the BSD 3-clause license (see LICENSE.txt)
+
+# ---------------------------------------------------------
+# Helper functions and utilities for deep learning models
+# ---------------------------------------------------------
+
+import numpy as np
+from matplotlib import pyplot as plt
+from sklearn.metrics import roc_auc_score
+
+from utils.make_data import * 
+
+def plot_1D_uncertainty(results, Y_test, data_index):
+    
+    plt.fill_between(list(range(len(results["Lower limit"][data_index]))), 
+                     results["Lower limit"][data_index].reshape(-1,), 
+                     results["Upper limit"][data_index].reshape(-1,), color="r", alpha=0.25)
+
+    plt.plot(results["Lower limit"][data_index], linestyle=":", linewidth=3, color="r")
+    plt.plot(results["Upper limit"][data_index], linestyle=":", linewidth=3, color="r")
+
+    plt.plot(Y_test[data_index], linestyle="--", linewidth=2, color="black")
+    plt.plot(results["Point predictions"][data_index], linewidth=3, color="r", Marker="o")
